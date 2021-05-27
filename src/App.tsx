329444450140d47b927be0abdce55c10a614c4ac
MohamedAhmed122 <mohamed.ahm9833@gmail.com>
2021-05-27 13:58:21 +0700

    LocationSearchInput : Places Auto Compelete

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import { Location } from "./typings";
+import LocationSearchInput from "./components/Header/LocationSearchInput";
 
 function App() {
   const [location, setLocation] = useState<Location>({
     lat: 1,
     lng: 1,
   });
-  const [isLocationChanged, setIsLocationChanged] = useState<boolean>(false);
+  const [isLocationChanged, setIsLocationChanged] = useState(false);
 
   return (
     <LocationContext.Provider
-      value={{ location, setLocation, isLocationChanged, setIsLocationChanged }}
+      value={{ isLocationChanged, setIsLocationChanged, location, setLocation }}
     >
       <Header />
       <div className="container">
         <Body />
+        <LocationSearchInput />
         <Footer />
       </div>
     </LocationContext.Provider>
---
+++ src/components/Header/LocationSearchInput/index.tsx
@@ -0,0 +1,71 @@
+import React, { useContext, useState } from "react";
+import LocationContext from "../../../locationContext";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import { Location } from '../../../typings/index';
+
+import './searchInputStyle.css'
+
+export default function LocationSearchInput() {
+  const { setLocation, setIsLocationChanged } = useContext(LocationContext);
+  const [address, setAddress] = useState<string>("");
+
+  const handleChange = (address: string) => {
+    setAddress(address);
+  };
+
+  const handleSelect = (address: string) => {
+    geocodeByAddress(address)
+      .then((results: any) => getLatLng(results[0]))
+      .then((latLng: Location) => {
+        setLocation(latLng);
+        setIsLocationChanged(true);
+        // setDisplayInput(false)
+      })
+      .catch((error:any) => console.error("Error", error));
+  };
+
+  return (
+    <PlacesAutocomplete
+      value={address}
+      onChange={handleChange}
+      onSelect={handleSelect}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div className="input" style={{ width: 400 }}>
+          <input
+            {...getInputProps({
+              placeholder: "Search Places ...",
+              className: "location-search-input",
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active
+                ? "suggestion-item--active"
+                : "suggestion-item";
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                : { backgroundColor: "#ffffff", cursor: "pointer" };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span style={{ padding: 10 }}>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
+  );
+}
+
